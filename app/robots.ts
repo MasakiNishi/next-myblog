ff6e4b360a9136d15fdb6eb6943e7aabc6010f9d
MasakiNishi <n24mfs@gmail.com>
2024-08-16 03:53:36 +0900

    [change] allow robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -6,8 +6,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      //   allow: "/",
-      disallow: "/",
+      allow: "/",
     },
     sitemap: `${SeoConst.domain}/sitemap.xml`,
   };
