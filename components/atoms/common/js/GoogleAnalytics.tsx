4dfb08f732a8507c1c387bb13c7bac398e06a4eb
MasakiNishi <n24mfs@gmail.com>
2024-08-18 23:30:18 +0900

    [fix] twitter, google js strategy

--- components/atoms/common/js/GoogleAnalytics.tsx
+++ components/atoms/common/js/GoogleAnalytics.tsx
@@ -36,7 +36,7 @@ const GoogleAnalytics = () => {
   return (
     <>
       <Script
-        strategy="lazyOnload"
+        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
       />
       <Script id="gtag-init" strategy="afterInteractive">
