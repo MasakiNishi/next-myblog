97ee8c1e04d8886545bc795327a9f5d4f9b9e3cc
MasakiNishi <n24mfs@gmail.com>
2024-08-21 00:59:37 +0900

    [add] via and related to twitter share

--- components/molecules/post/PostShare.tsx
+++ components/molecules/post/PostShare.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+// constant
+import SeoConst from "../../../constants/SeoConst";
+// component
 import {
   TwitterShareButton,
   FacebookShareButton,
@@ -14,13 +17,19 @@ import {
 } from "react-share";
 
 const PostShare = ({ slug, title }: { slug: string; title: string }) => {
+  const twitterId = SeoConst.twitterId.slice(1);
   return (
     <div className="text-center my-8">
       <p className="text-lg sm:text-xl lg:text-xl text-gray-600 my-8">
         シェアする
       </p>
       <div className="flex justify-center space-x-4">
-        <TwitterShareButton url={slug} title={title}>
+        <TwitterShareButton
+          url={slug}
+          title={title}
+          via={twitterId}
+          related={[`${twitterId}`]}
+        >
           <TwitterIcon size={35} round={true} />
         </TwitterShareButton>
         <FacebookShareButton url={slug} title={title}>
